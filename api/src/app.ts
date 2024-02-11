e0bcfd6b1c02b28b905c46e00db4455aa294fa77
Chongyi Zheng <git@zcy.dev>
2024-02-11 15:22:29 -0500

    Refactor

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,21 +6,6 @@ import dbConfig from './db.config'
 import { Role } from './models/roleSchema'
 import routes from './routes/index'
 
-/* Creating an express app on port 8080 */
-const app = express()
-const port = 3000
-
-/* Initializing cors */
-var corsOptions = {
-  origin: 'http://localhost:4200',
-}
-
-app.use(cors(corsOptions))
-
-/* Allowing express to use jsons */
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
 mongoose
   .connect(dbConfig.url, {
     // useNewUrlParser: true,
@@ -61,6 +46,17 @@ function initialize() {
 }
 
 initialize()
+
+const app = express()
+const port = 3000
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/', routes)
 app.listen(port, () => {
