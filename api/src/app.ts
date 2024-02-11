2ab2741d4919c87ccd22e6f4cf544ddf3a012e4a
Chongyi Zheng <git@zcy.dev>
2024-02-11 15:21:33 -0500

    Fix errors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,9 @@
 import cors from 'cors'
 import express from 'express'
+import mongoose from 'mongoose'
+import dbConfig from './db.config'
 
-import * as db from './database'
+import { Role } from './models/roleSchema'
 import routes from './routes/index'
 
 /* Creating an express app on port 8080 */
@@ -19,8 +21,46 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-db.connect()
-db.initialize()
+mongoose
+  .connect(dbConfig.url, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the database!')
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database!', err)
+    process.exit()
+  })
+
+function initialize() {
+  Role.estimatedDocumentCount((err: Error, count: number) => {
+    if (!err && count === 0) {
+      new Role({
+        name: 'user',
+      }).save((err) => {
+        if (err) {
+          console.log('error', err)
+        }
+
+        console.log("added 'user' to roles collection")
+      })
+
+      new Role({
+        name: 'admin',
+      }).save((err) => {
+        if (err) {
+          console.log('error', err)
+        }
+
+        console.log("added 'admin' to roles collection")
+      })
+    }
+  })
+}
+
+initialize()
 
 app.use('/', routes)
 app.listen(port, () => {
